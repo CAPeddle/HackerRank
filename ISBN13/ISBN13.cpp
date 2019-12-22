b687ba3250dd0ee8854544dfbf951dd38a2088ab
Christopher Peddle <ChrisAPeddle@gmail.com>
2019-12-22 20:32:46 +0200

    Ported to class and added static method

--- ISBN13/ISBN13.cpp
+++ ISBN13/ISBN13.cpp
@@ -9,23 +9,107 @@
 #include <sstream>
 #include <vector>
 
+class ISBN13
+{
+	static constexpr size_t ISBN_LENGTH = 13;
+	//https://en.m.wikipedia.org/wiki/International_Standard_Book_Number#Check_digits
+public:
+	ISBN13() = default;
+		
+	bool SetBase(std::string twelve_digits); 	
+	std::string GetComplete();
 
-auto sum_by_three_algo = [](uint8_t a, uint8_t b) {
-	static bool applyThree = false;
+	static std::string Complete(std::string twelve_digits)
+	{
+		std::unique_ptr<ISBN13> p = std::make_unique<ISBN13>();
+		if (p->SetBase(twelve_digits)) {
+			return p->GetComplete();
+		} else {
+			return std::string();
+		}
+	}
 
-	if (applyThree)
+private:
+	std::string _digits;
+	std::vector<uint8_t> _isbn;
+
+	uint8_t GetCrc(uint8_t sum_of_three) noexcept
 	{
-		applyThree = false;
-		return  a + 3 * b;
+		auto modulus = sum_of_three % 10;
+		auto ten_and_modulus = 10 - modulus;
+		return ten_and_modulus != 10 ? ten_and_modulus : 0;
 	}
-	else
+
+	std::vector<uint8_t> GetVector()
 	{
-		applyThree = true;
+		_isbn.resize(ISBN_LENGTH);
+		
+		char digit;
+		for (size_t i = 0; i < (ISBN_LENGTH - 1); ++i) {
+			digit = _digits[i];
+			const auto temp = static_cast<uint8_t>(std::strtoul(&digit, nullptr, 10));
+			_isbn[i] = temp;			
+		}
+		return _isbn;
 	}
-	
-	return a + b;
+
+	uint8_t GetSumByThree(std::vector<uint8_t> isbn)
+	{
+		if (isbn.empty()) return 0;
+
+		if (isbn.size() % 2 != 0)
+		{
+			auto current_last = isbn.back();
+			isbn.pop_back();
+			return current_last + GetSumByThree(isbn);
+		}
+		else
+		{
+			auto current_last = isbn.back();
+			isbn.pop_back();
+			return current_last * 3 + GetSumByThree(isbn);
+		}
+	}
+
+	std::string GetISBN13()
+	{	
+		return std::accumulate(std::next(_isbn.begin()), _isbn.end(),
+			std::to_string(_isbn[0]),
+			[](std::string a, uint8_t b) {
+				return std::move(a) + std::to_string(b);
+			});
+	}
 };
 
+bool ISBN13::SetBase(std::string digits)
+{
+	if (digits.length() != (ISBN_LENGTH-1)) return false;
+
+	for (const auto& digit : digits)
+	{
+		if (!isdigit(digit)) return false;
+	}
+		
+	_digits = digits;
+
+	return true;
+}
+
+std::string ISBN13::GetComplete()
+{
+	auto twelve_digit = GetVector();
+	int sum = GetSumByThree(_isbn);
+	int check_digit = GetCrc(sum);
+	_isbn[ISBN_LENGTH - 1] = check_digit;
+	return GetISBN13();
+}
+
+uint8_t get_crc(uint8_t sum_of_three)
+{
+	auto modulus = sum_of_three % 10;
+	auto ten_and_modulus = 10 - modulus;
+	return ten_and_modulus != 10 ? ten_and_modulus : 0;
+}
 
 uint8_t sum_by_three(std::vector<uint8_t> &isbn)
 {
@@ -45,11 +129,11 @@ uint8_t sum_by_three(std::vector<uint8_t> &isbn)
 	}
 }
 
-std::vector<uint8_t> GetISBN(const std::string digits)
+std::vector<uint8_t> GetISBN(const std::string &digits)
 {
 	std::vector<uint8_t> isbn (digits.length(), 0);
 		
-	if (digits.length() != 12) return isbn;
+	if (digits.length() < 12) return isbn;
 	
 	char digit;
 	for (size_t i = 0; i < 12; ++i) {
@@ -70,32 +154,43 @@ std::vector<uint8_t> GetISBN(const std::string digits)
 
 int main()
 {
-  std::cout << "Enter 12 digits\n";
+	std::cout << "Enter 12 digits\n";
+	std::string digits;
 
-#ifdef TEST	
+#define TEST
+#ifndef TEST	
 	std::getline(std::cin, digits);
 #else
-	std::string digits = "978014300723";
+	digits = "978014300723";
+	//digits = "1234567890123";
 #endif
 
 	for (size_t i = 0; i < 3; i++)
 	{
 		auto isbn = GetISBN(digits);
-				
-		int accu_sum = std::accumulate(isbn.begin(), isbn.end(), 0);
-
 		int sum = sum_by_three(isbn);
+		int check_digit = get_crc(sum);
 		
-		auto s = std::accumulate(isbn.begin(), isbn.end(),
-			0, // start with first element
-			sum_by_three_algo);
-			
 		std::cout << '\n';
 		for (const auto& s : isbn)
 			std::cout << std::to_string(s) << ' ';
 
-		std::cout << '\n' << s << '\n' << accu_sum << '\n' << sum;
+		std::cout << '\n' << sum << '\n' << check_digit;
 	}
+
+	std::cout << '\n' << "Class impelementation";
+	ISBN13 testisbnclass;
+	if (testisbnclass.SetBase(digits)) {
+		std::cout << '\n' << testisbnclass.GetComplete();
+	}
+	else
+	{
+		std::cout << '\n' << "Fail on digits";
+	}
+	
+	std::cout << '\n' << "Static impelementation";
+		
+	std::cout << '\n' << ISBN13::Complete(digits);
 	
 }
 
---
+++ NativeTest/NativeTest.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace NativeTest
+{
+	TEST_CLASS(NativeTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
